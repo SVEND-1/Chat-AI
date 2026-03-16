ffad1bf6e9d26a14053f3447cd853ed2cdb54a4e
Bogdan <minecraftspeed3@gmail.com>
2026-03-16 23:49:10 +0300

    Added special user response for support tickets

--- src/main/java/org/example/chatai/supportTickets/api/dto/responses/SupportTicketResponse.java
+++ src/main/java/org/example/chatai/supportTickets/api/dto/responses/SupportTicketResponse.java
@@ -1,14 +1,15 @@
 package org.example.chatai.supportTickets.api.dto.responses;
 
 import org.example.chatai.supportTickets.db.enums.SupportStatus;
+import org.example.chatai.users.api.dto.users.response.UserSupportTicketResponse;
 import org.example.chatai.users.db.UserEntity;
 
 import java.time.LocalDateTime;
 
 public record SupportTicketResponse(
         Long id,
-        UserEntity user,
-        UserEntity support,
+        UserSupportTicketResponse user,
+        UserSupportTicketResponse support,
         String title,
         SupportStatus status,
         LocalDateTime createdAt,
