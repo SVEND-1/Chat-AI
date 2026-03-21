ff6aaf9b1fb2c364d2f4b39f76a52d7570532d57
SVEND-1 <s5090@inbox.ru>
2026-03-21 19:24:42 +0300

    Написал документацию Добавил индексы в бд

--- src/main/java/org/example/chatai/roleApplication/api/RoleController.java
+++ src/main/java/org/example/chatai/roleApplication/api/RoleController.java
@@ -1,5 +1,6 @@
 package org.example.chatai.roleApplication.api;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.example.chatai.roleApplication.api.dto.request.RoleCreateRequest;
 import org.example.chatai.roleApplication.api.dto.response.RoleResponse;
@@ -15,11 +16,13 @@ import java.util.List;
 public class RoleController {
     private final RoleService roleService;
 
+    @Operation(summary = "Получить все заявки пользователя на смену роли")
     @GetMapping
     public ResponseEntity<List<RoleResponse>> getRoles() {
         return ResponseEntity.ok(roleService.findAllByUser());
     }
 
+    @Operation(summary = "Создать заявку на смену роли")
     @PostMapping
     public ResponseEntity<String> created(@RequestBody RoleCreateRequest request) {
         return ResponseEntity.ok(roleService.save(request));
